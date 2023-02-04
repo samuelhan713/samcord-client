16677fa7000dd8cfa706a11a878e8edd2311d2ac
Samuel Han <60247381+Sammy713@users.noreply.github.com>
2023-02-05 02:03:15 +0900

    Created socket connection with React

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Home from './components/Home'
 import Login from './components/Login'
+import { io } from 'socket.io-client';
 
 function App() {
+
+  const socket = io('http://localhost:4000');
+  useEffect(() => { }, []);
 
   const userInfo = {
     username: "samuel.han@gmail.com"
